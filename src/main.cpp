5d736f1757bc6dfe67157fb6eed8579e57d754bd
LNJ <git@lnj.li>
2017-01-05 21:08:28 +0100

    Pass version and name variables from cmake to C++ (#43)
    New global variables in C++ are:
    	APPLICATION_NAME
    	APPLICATION_DISPLAY_NAME
    	APPLICATION_ID
    	VERSION_STRING

--- src/main.cpp
+++ src/main.cpp
@@ -1,21 +1,25 @@
+// Qt
 #include <QGuiApplication>
 #include <QQmlApplicationEngine>
 #include <QQuickView>
 #include <QQmlContext>
 #include <QtQml>
-
+// Swiften
 #include "Swiften/EventLoop/Qt/QtEventLoop.h"
-
+// Kaidan
 #include "Kaidan.h"
 #include "RosterController.h"
 #include "RosterItem.h"
 
 int main(int argc, char *argv[])
 {
-	qmlRegisterType<RosterController>( "harbour.kaidan", 1, 0, "RosterController");
-	qmlRegisterType<RosterItem>( "harbour.kaidan", 1, 0, "RosterItem");
+	qmlRegisterType<RosterController>(APPLICATION_ID, 1, 0, "RosterController");
+	qmlRegisterType<RosterItem>(APPLICATION_ID, 1, 0, "RosterItem");
 
 	QGuiApplication app(argc, argv);
+	QGuiApplication::setApplicationName(APPLICATION_NAME);
+	QGuiApplication::setApplicationDisplayName(APPLICATION_DISPLAY_NAME);
+	QGuiApplication::setApplicationVersion(VERSION_STRING);
 
 	QtEventLoop eventLoop;
 	BoostNetworkFactories networkFactories(&eventLoop);

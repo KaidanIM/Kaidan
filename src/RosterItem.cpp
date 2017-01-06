16135f48537bd82da58532eba0561b58595a74e1
LNJ <git@lnj.li>
2017-01-06 16:45:44 +0100

    Add copyright notices in all files

--- src/RosterItem.cpp
+++ src/RosterItem.cpp
@@ -1,3 +1,22 @@
+/*
+ *  Kaidan - Cross platform XMPP client
+ *
+ *  Copyright (C) 2016 geobra <s.g.b@gmx.de>
+ *
+ *  Kaidan is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  Kaidan is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with Kaidan. If not, see <http://www.gnu.org/licenses/>.
+ */
+
 #include "RosterItem.h"
 
 RosterItem::RosterItem(QObject *parent) : QObject(parent), subscription_(None), jid_(""), name_("")

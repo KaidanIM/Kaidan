3445b982c31a481f41022cd265a643481cba3593
LNJ <git@lnj.li>
2017-01-07 19:47:49 +0100

    Add i18n support; Add German l10n

---
+++ i18n/de_DE.ts
@@ -0,0 +1,60 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="de_DE">
+<context>
+    <name>ChatPage</name>
+    <message>
+        <source>Compose message</source>
+        <translation>Nachricht verfassen</translation>
+    </message>
+    <message>
+        <source>Send</source>
+        <translation>Senden</translation>
+    </message>
+</context>
+<context>
+    <name>LoginPage</name>
+    <message>
+        <source>Your Jabber-ID:</source>
+        <translation>Deine Jabber-ID:</translation>
+    </message>
+    <message>
+        <source>user@example.org</source>
+        <translation>user@beispiel.org</translation>
+    </message>
+    <message>
+        <source>Your Password:</source>
+        <translation>Dein Passwort:</translation>
+    </message>
+    <message>
+        <source>Password</source>
+        <translation>Passwort</translation>
+    </message>
+    <message>
+        <source>Connect</source>
+        <translation>Verbinden</translation>
+    </message>
+    <message>
+        <source>&lt;i&gt;Connecting...&lt;/i&gt;</source>
+        <translation>&lt;i&gt;Verbinden...&lt;/i&gt;</translation>
+    </message>
+    <message>
+        <source>Retry</source>
+        <translation>Erneut Versuchen</translation>
+    </message>
+</context>
+<context>
+    <name>RosterPage</name>
+    <message>
+        <source>Contacts</source>
+        <translation>Kontakte</translation>
+    </message>
+</context>
+<context>
+    <name>main</name>
+    <message>
+        <source>Successfully parsed command line input.</source>
+        <translation>Kommandozeileneingaben erfolgreich geparst.</translation>
+    </message>
+</context>
+</TS>
--- src/RosterItem.h
+++ src/RosterItem.h
@@ -23,7 +23,8 @@
 #include <QObject>
 
 enum Subscription
-{ None,
+{
+	None,
 	To,
 	From,
 	Both,

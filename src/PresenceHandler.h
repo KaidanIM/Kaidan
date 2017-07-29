8b22530ceca3a01c0a70e203f599bccb7f70ee0c
LNJ <git@lnj.li>
2017-07-29 11:41:53 +0200

    Rewrite PresenceController -> PresenceHandler using gloox
    The Sub/Unsub management in gloox is done in the Roster/RosterListener/RosterUpdater.
    Therefore the PresenceHandler is currently useless. (Probably will change in the future)
    Sub requests are accepted automatically and the user will be unsubbed on incoming unsub
    requests (when the other entity unsubs from the user).

---
+++ src/PresenceHandler.h
@@ -0,0 +1,39 @@
+/*
+ *  Kaidan - A user-friendly XMPP client for every device!
+ *
+ *  Copyright (C) 2017 LNJ <git@lnj.li>
+ *
+ *  Kaidan is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  Kaidan is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with Kaidan. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#ifndef PRESENCEHANDLER_H
+#define PRESENCEHANDLER_H
+
+#include <QObject>
+#include <gloox/client.h>
+#include <gloox/presence.h>
+#include <gloox/presencehandler.h>
+
+class PresenceHandler : public gloox::PresenceHandler
+{
+public:
+	PresenceHandler(gloox::Client *client);
+	~PresenceHandler();
+	virtual void handlePresence(const gloox::Presence &presence);
+
+private:
+	gloox::Client *client;
+};
+
+#endif // PRESENCEHANDLER_H

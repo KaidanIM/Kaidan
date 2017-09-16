bb7e26d6f42f831791e99cf6fcdb7124ddcf0c44
Muhammad Nur Hidayat Yasuyoshi (MNH48.com) <muhdnurhidayat96@yahoo.com>
2017-09-17 02:08:25 +0800

    i18n: Add Malay translation (#129)

---
+++ i18n/ms.ts
@@ -0,0 +1,153 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="ja">
+<context>
+    <name>AboutPage</name>
+    <message>
+        <source>About</source>
+        <translation>Latar Belakang</translation>
+    </message>
+    <message>
+        <source>A simple, user-friendly Jabber/XMPP client</source>
+        <translation>Klien Jabber/XMPP yang mudah, mesra pengguna</translation>
+    </message>
+    <message>
+        <source>License:</source>
+        <translation>Lesen:</translation>
+    </message>
+    <message>
+        <source>Sourcecode on Github</source>
+        <translation>Kod sumber di Github</translation>
+    </message>
+    <message>
+        <source>Close</source>
+        <translation>Tutup</translation>
+    </message>
+</context>
+<context>
+    <name>ChatPage</name>
+    <message>
+        <source>Compose message</source>
+        <translation>Tulis mesej</translation>
+    </message>
+    <message>
+        <source>Send</source>
+        <translation>Hantar</translation>
+    </message>
+</context>
+<context>
+    <name>GlobalDrawer</name>
+    <message>
+        <source>Logout</source>
+        <translation>Log keluar</translation>
+    </message>
+    <message>
+        <source>About</source>
+        <translation>Latar Belakang</translation>
+    </message>
+    <message>
+        <source>Add new contact</source>
+        <translation>Tambah kenalan baru</translation>
+    </message>
+</context>
+<context>
+    <name>LoginPage</name>
+    <message>
+        <source>Your Jabber-ID:</source>
+        <translation>Jabber-ID anda：</translation>
+    </message>
+    <message>
+        <source>user@example.org</source>
+        <translation>contoh＠emel.com</translation>
+    </message>
+    <message>
+        <source>Your Password:</source>
+        <translation>Kata Laluan Anda：</translation>
+    </message>
+    <message>
+        <source>Password</source>
+        <translation>Kata Laluan</translation>
+    </message>
+    <message>
+        <source>Retry</source>
+        <translation>Cuba Lagi</translation>
+    </message>
+    <message>
+        <source>Connect</source>
+        <translation>Hubungi</translation>
+    </message>
+    <message>
+        <source>Connecting...</source>
+        <translation>Menghubungi...</translation>
+    </message>
+    <message>
+        <source>Login</source>
+        <translation>Log Masuk</translation>
+    </message>
+    <message>
+        <source>Log in to your Jabber Account</source>
+        <translation>Log masuk akaun Jabber anda</translation>
+    </message>
+    <message>
+        <source>Your diaspora*-ID:</source>
+        <translation>ID diaspora* anda:</translation>
+    </message>
+    <message>
+        <source>user@diaspora.pod</source>
+        <translation>nama@diaspora.pod</translation>
+    </message>
+</context>
+<context>
+    <name>RosterAddContactSheet</name>
+    <message>
+        <source>Nickname:</source>
+        <translation>Nama panggilan:</translation>
+    </message>
+    <message>
+        <source>Jabber-ID:</source>
+        <translation>ID Jabber</translation>
+    </message>
+    <message>
+        <source>user@example.org</source>
+        <translation>contoh＠emel.com</translation>
+    </message>
+    <message>
+        <source>Cancel</source>
+        <translation>Batal</translation>
+    </message>
+    <message>
+        <source>Add</source>
+        <translation>Tambah</translation>
+    </message>
+    <message>
+        <source>Add new contact</source>
+        <translation>Tambah kenalan baru</translation>
+    </message>
+</context>
+<context>
+    <name>RosterPage</name>
+    <message>
+        <source>Contacts</source>
+        <translation>Kenalan</translation>
+    </message>
+</context>
+<context>
+    <name>RosterRemoveContactSheet</name>
+    <message>
+        <source>Do you really want to delete the contact &quot;%1&quot; from your roster?</source>
+        <translation>Adakah anda ingin memadam kenalan &quot;%1&quot; daripada roster anda?</translation>
+    </message>
+    <message>
+        <source>Delete contact</source>
+        <translation>Padam kenalan</translation>
+    </message>
+    <message>
+        <source>Cancel</source>
+        <translation>Batal</translation>
+    </message>
+    <message>
+        <source>Delete</source>
+        <translation>Padam</translation>
+    </message>
+</context>
+</TS>

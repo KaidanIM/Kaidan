704a138fb1164ff9515320e6a1a22b525f6a8cd2
JBBgameich <jbbgameich@gmail.com>
2017-11-28 21:35:33 +0100

    travis x1

--- utils/travis-dependencies.sh
+++ utils/travis-dependencies.sh
@@ -4,9 +4,9 @@ echo "*****************************************"
 echo "Fetching dependencies"
 echo "*****************************************"
 
-sudo apt update
+sudo apt-get update
 
-sudo apt install software-properties-common dirmngr gnupg ca-certificates wget -y
+sudo apt-get install software-properties-common dirmngr gnupg ca-certificates wget -y
 
 # Trusty backports
 sudo bash -c "echo deb http://archive.ubuntu.com/ubuntu trusty-backports main restricted universe multiverse >> /etc/apt/sources.list"
@@ -18,12 +18,11 @@ sudo bash -c "echo deb https://raw.githubusercontent.com/kaidanim/packages/maste
 # Qt 5.9 repository
 sudo add-apt-repository ppa:beineri/opt-qt593-trusty -y
 
-sudo apt update
+sudo apt-get update
 sudo apt install cmake \
             build-essential \
             ninja-build \
             zlib1g-dev \
-            libgloox-dev \
             qt59base \
             qt59script \
             qt59declarative \
@@ -41,6 +40,6 @@ sudo cp -Rf kf5-release/* /opt/qt*/
 # Gloox
 wget -c "https://github.com/JBBgameich/precompiled-kf5-linux/releases/download/KF5.40/libgloox17_1.0.20-1_amd64.deb"
 wget -c "https://github.com/JBBgameich/precompiled-kf5-linux/releases/download/KF5.40/libgloox-dev_1.0.20-1_amd64.deb"
-sudo dpkg -i libgloox-*.deb
+sudo dpkg -i libgloox*.deb
 rm libgloox*.deb
 sudo apt install -f -y

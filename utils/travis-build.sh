da1143eb4bc2d1f437455f976cc0b74be74bfb70
JBBgameich <jbbgameich@gmail.com>
2017-11-28 21:39:57 +0100

    travis x2

--- utils/travis-build.sh
+++ utils/travis-build.sh
@@ -4,8 +4,6 @@ echo "*****************************************"
 echo "Building Kaidan binary"
 echo "*****************************************"
 
-source /opt/qt*/bin/qt*-env.sh
-
 mkdir ../build; cd ../build/
 
 cmake .. -GNinja -DI18N=1

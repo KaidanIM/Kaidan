236f4f1f817376ca99141e96e9a84eec5b9f1320
JBBgameich <jbbgameich@gmail.com>
2017-11-29 17:22:19 +0100

    Build AppImage on Travis-ci

--- utils/AppImage.sh
+++ utils/AppImage.sh
@@ -13,6 +13,13 @@ fi
 # configure buildsystem and environment for AppDir installation
 if ! [ -d ../build ]; then mkdir ../build; fi
 cd ../build
+
+# Find Qt5
+if [ -d /opt/qt59/qml ]; then
+    export QMLDIR="/opt/qt59/qml"
+else if [ -d /usr/lib/x86_64-linux-gnu/qt5/qml/ ]; then
+    export QMLDIR="/usr/lib/x86_64-linux-gnu/qt5/qml/"
+fi
 export QT_SELECT=qt5
 
 # Build kaidan
@@ -22,7 +29,7 @@ DESTDIR=../AppDir cmake --build . --target install
 cd ..
 
 # Copy dependencies into AppDir
-./utils/linuxdeployqt-continuous-x86_64.AppImage $PWD/AppDir/usr/share/applications/kaidan.desktop -qmldir=/usr/lib/x86_64-linux-gnu/qt5/qml/ -bundle-non-qt-libs
+./utils/linuxdeployqt-continuous-x86_64.AppImage $PWD/AppDir/usr/share/applications/kaidan.desktop -qmldir=$QMLDIR -bundle-non-qt-libs
 
 # Create AppImage
 ./utils/linuxdeployqt-continuous-x86_64.AppImage $PWD/AppDir/usr/share/applications/kaidan.desktop -appimage

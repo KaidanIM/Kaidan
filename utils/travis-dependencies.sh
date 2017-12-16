7e41994685013389cac964ee6b977642c668d81a
JBBgameich <jbbgameich@gmail.com>
2017-12-16 22:15:16 +0100

    Upgrade KF5 to 5.41

--- utils/travis-dependencies.sh
+++ utils/travis-dependencies.sh
@@ -31,8 +31,8 @@ sudo apt install cmake \
             -y -t trusty-backports
 
 # KF5
-wget -c "https://github.com/JBBgameich/precompiled-kf5-linux/releases/download/KF5.40/kf5.40-gcc6-linux64-release.tar.xz"
-tar xf kf5.40-gcc6-linux64-release.tar.xz
+wget -c "https://github.com/JBBgameich/precompiled-kf5-linux/releases/download/KF5.41/kf5.41-gcc6-linux64-release.tar.xz"
+tar xf kf5.41-gcc6-linux64-release.tar.xz
 sudo cp -rf kf5-release/* /opt/qt*/
 sudo mkdir -p /opt/qt59/qml/org/kde/
 sudo mv /opt/qt59/lib/x86_64-linux-gnu/qml/org/kde/kirigami.2 /opt/qt59/qml/org/kde/

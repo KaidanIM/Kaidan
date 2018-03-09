f395dce95d1d6ed46c1e420a606484caacf36908
LNJ <git@lnj.li>
2018-03-09 17:49:05 +0100

    cmake include gloox include dir

--- cmake/FindGloox.cmake
+++ cmake/FindGloox.cmake
@@ -2,7 +2,7 @@
 # Find GLOOX headers, libraries and the answer to all questions.
 #
 #  GLOOX_FOUND               True if gloox got found
-#  GLOOX_INCLUDE_DIR        Location of gloox headers 
+#  GLOOX_INCLUDE_DIR         Location of gloox headers
 #  GLOOX_LIBRARIES           List of libaries to use gloox 
 #
 # Copyright (c) 2009 Nigmatullin Ruslan <euroelessar@gmail.com>
@@ -17,6 +17,7 @@ FIND_LIBRARY( GLOOX_LIBRARIES gloox )
 
 if( GLOOX_LIBRARIES AND GLOOX_INCLUDE_DIR )
 	message( STATUS "Found gloox: ${GLOOX_LIBRARIES}" )
+	include_directories(${GLOOX_INCLUDE_DIR})
 	set( GLOOX_FOUND 1 )
 else( GLOOX_LIBRARIES AND GLOOX_INCLUDE_DIR )
 	message( STATUS "Could NOT find gloox" )

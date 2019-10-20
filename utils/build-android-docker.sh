3ea3ca48748ba6d6037ed4a8ac97c64f6783b451
Jonah Brüchert <jbb.prv@gmx.de>
2019-10-20 14:56:13 +0200

    android: Enable KNotifications

--- utils/build-android-docker.sh
+++ utils/build-android-docker.sh
@@ -15,20 +15,20 @@ sudo apt update
 sudo apt -y install inkscape optipng
 
 # Build dependencies
-GIT_EXTRA="--branch ${KF5_VERSION}" /opt/helpers/build-kde-dependencies --withProject kirigami
+GIT_EXTRA="--branch ${KF5_VERSION}" /opt/helpers/build-kde-dependencies --withProject kirigami knotifications
 /opt/helpers/build-cmake qxmpp https://github.com/qxmpp-project/qxmpp.git -DBUILD_EXAMPLES=OFF -DBUILD_TESTS=OFF
 /opt/helpers/build-cmake zxing-cpp https://github.com/nu-book/zxing-cpp.git
 
 # Trick the script to not clone kaidan once again
 if ! [ -f "${KAIDAN_SOURCES}/kaidan/" ]; then
-    ln -s ${KAIDAN_SOURCES} ${PWD}/src/kaidan 
+    ln -s ${KAIDAN_SOURCES} ${PWD}/src/kaidan
 fi
 
 # Build Kaidan
-/opt/helpers/build-cmake ${CI_PROJECT_NAME} ${CI_REPOSITORY_URL} -DQTANDROID_EXPORTED_TARGET=kaidan -DANDROID_APK_DIR=${CI_PROJECT_DIR}/src/${CI_PROJECT_NAME}/misc/android -DI18N=1 -DUSE_KNOTIFICATIONS=OFF
+/opt/helpers/build-cmake ${CI_PROJECT_NAME} ${CI_REPOSITORY_URL} -DQTANDROID_EXPORTED_TARGET=kaidan -DANDROID_APK_DIR=${CI_PROJECT_DIR}/src/${CI_PROJECT_NAME}/misc/android -DI18N=1 -DUSE_KNOTIFICATIONS=ON
 
 # pack apk
 ${CI_PROJECT_DIR}/src/${CI_PROJECT_NAME}/utils/render-logos.sh
 /opt/helpers/create-apk ${CI_PROJECT_NAME}
 
-mv ${CI_PROJECT_DIR}/build/${CI_PROJECT_NAME}/kaidan_build_apk/build/outputs/apk/kaidan_build_apk-debug.apk ${CI_PROJECT_DIR}
+mv ${CI_PROJECT_DIR}/build/${CI_PROJECT_NAME}/kaidan_build_apk/build/outputs/apk/debug/kaidan_build_apk-debug.apk ${CI_PROJECT_DIR}

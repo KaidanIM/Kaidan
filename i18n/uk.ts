9950b44f4a161e8db51f13ec34ce2c08f5f0c654
Yuri Chornoivan <yurchor@ukr.net>
2019-11-26 22:05:20 +0100

    Translate using Weblate (Ukrainian)
    Currently translated at 100.0% (111 of 111 strings)
    
    Add translation using Weblate (Ukrainian)

---
+++ i18n/uk.ts
@@ -0,0 +1,518 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="uk">
+<context>
+    <name>AboutDialog</name>
+    <message>
+        <source>A simple, user-friendly Jabber/XMPP client</source>
+        <translation>Простий дружній до користувачів клієнт Jabber/XMPP</translation>
+    </message>
+    <message>
+        <source>License:</source>
+        <translation>Ліцензування:</translation>
+    </message>
+    <message>
+        <source>View source code online</source>
+        <translation>Переглянути початковий код у інтернеті</translation>
+    </message>
+</context>
+<context>
+    <name>ChangePassword</name>
+    <message>
+        <source>Change password</source>
+        <translation>Змінити пароль</translation>
+    </message>
+    <message>
+        <source>Current password:</source>
+        <translation>Поточний пароль:</translation>
+    </message>
+    <message>
+        <source>New password:</source>
+        <translation>Новий пароль:</translation>
+    </message>
+    <message>
+        <source>New password (repeat):</source>
+        <translation>Новий пароль (ще раз):</translation>
+    </message>
+    <message>
+        <source>New passwords do not match.</source>
+        <translation>Вказані нові паролі не збігаються.</translation>
+    </message>
+    <message>
+        <source>Current password is invalid.</source>
+        <translation>Поточний пароль є некоректним.</translation>
+    </message>
+    <message>
+        <source>You need to be connected to change your password.</source>
+        <translation>Вам слід встановити з&apos;єднання, щоб змінити ваш пароль.</translation>
+    </message>
+    <message>
+        <source>After changing your password, you will need to reenter it on all your other devices.</source>
+        <translation>Після зміни вашого пароля вам доведеться повторно ввести його на усіх інших ваших пристроях для спілкування.</translation>
+    </message>
+    <message>
+        <source>Cancel</source>
+        <translation>Скасувати</translation>
+    </message>
+    <message>
+        <source>Change</source>
+        <translation>Змінити</translation>
+    </message>
+</context>
+<context>
+    <name>ChatMessage</name>
+    <message>
+        <source>Copy Message</source>
+        <translation>Скопіювати повідомлення</translation>
+    </message>
+    <message>
+        <source>Edit Message</source>
+        <translation>Редагування повідомлення</translation>
+    </message>
+    <message>
+        <source>Spoiler</source>
+        <translation>Спойлер</translation>
+    </message>
+    <message>
+        <source>Download</source>
+        <translation>Отримати</translation>
+    </message>
+</context>
+<context>
+    <name>ChatPage</name>
+    <message>
+        <source>Compose message</source>
+        <translation>Створити повідомлення</translation>
+    </message>
+    <message>
+        <source>Image</source>
+        <translation>Зображення</translation>
+    </message>
+    <message>
+        <source>Video</source>
+        <translation>Відео</translation>
+    </message>
+    <message>
+        <source>Audio</source>
+        <translation>Звук</translation>
+    </message>
+    <message>
+        <source>Document</source>
+        <translation>Документ</translation>
+    </message>
+    <message>
+        <source>Other file</source>
+        <translation>Інший файл</translation>
+    </message>
+    <message>
+        <source>Select a file</source>
+        <translation>Виберіть файл</translation>
+    </message>
+    <message>
+        <source>Send a spoiler message</source>
+        <translation>Надіслати повідомлення-спойлер</translation>
+    </message>
+    <message>
+        <source>Spoiler hint</source>
+        <translation>Підказка до спойлера</translation>
+    </message>
+</context>
+<context>
+    <name>DownloadJob</name>
+    <message>
+        <source>Could not save file: %1</source>
+        <translation>Не вдалося зберегти файл %1</translation>
+    </message>
+    <message>
+        <source>Download failed: %1</source>
+        <translation>Невдала спроба отримання: %1</translation>
+    </message>
+</context>
+<context>
+    <name>EmojiPicker</name>
+    <message>
+        <source>Favorites</source>
+        <translation>Улюблене</translation>
+    </message>
+    <message>
+        <source>People</source>
+        <translation>Люди</translation>
+    </message>
+    <message>
+        <source>Nature</source>
+        <translation>Природа</translation>
+    </message>
+    <message>
+        <source>Food</source>
+        <translation>Їжа</translation>
+    </message>
+    <message>
+        <source>Activity</source>
+        <translation>Діяльність</translation>
+    </message>
+    <message>
+        <source>Travel</source>
+        <translation>Подорожі</translation>
+    </message>
+    <message>
+        <source>Objects</source>
+        <translation>Об&apos;єкти</translation>
+    </message>
+    <message>
+        <source>Symbols</source>
+        <translation>Символи</translation>
+    </message>
+    <message>
+        <source>Flags</source>
+        <translation>Прапори</translation>
+    </message>
+    <message>
+        <source>Search</source>
+        <translation>Пошук</translation>
+    </message>
+    <message>
+        <source>Search emoji</source>
+        <translation>Шукати емодзі</translation>
+    </message>
+</context>
+<context>
+    <name>EmptyChatPage</name>
+    <message>
+        <source>Please select a chat to start messaging</source>
+        <translation>Будь ласка, виберіть спілкування, щоб розпочати обмін повідомленнями</translation>
+    </message>
+</context>
+<context>
+    <name>FileChooser</name>
+    <message>
+        <source>Select a file</source>
+        <translation>Виберіть файл</translation>
+    </message>
+</context>
+<context>
+    <name>FileChooserMobile</name>
+    <message>
+        <source>Go to parent folder</source>
+        <translation>Перейти до батьківської теки</translation>
+    </message>
+    <message>
+        <source>Close</source>
+        <translation>Закрити</translation>
+    </message>
+</context>
+<context>
+    <name>GlobalDrawer</name>
+    <message>
+        <source>Log out</source>
+        <translation>Вийти</translation>
+    </message>
+    <message>
+        <source>About</source>
+        <translation>Про програму</translation>
+    </message>
+    <message>
+        <source>Invite friends</source>
+        <translation>Запросити друзів</translation>
+    </message>
+    <message>
+        <source>Invitation link copied to clipboard</source>
+        <translation>Посилання на запрошення скопійовано до буфера обміну даними</translation>
+    </message>
+    <message>
+        <source>Settings</source>
+        <translation>Параметри</translation>
+    </message>
+</context>
+<context>
+    <name>Kaidan</name>
+    <message>
+        <source>The link will be opened after you have connected.</source>
+        <extracomment>The link is an XMPP-URI (i.e. &apos;xmpp:kaidan@muc.kaidan.im?join&apos; for joining a chat)</extracomment>
+        <translation>Посилання буде відкрито після встановлення з&apos;єднання.</translation>
+    </message>
+</context>
+<context>
+    <name>LoginPage</name>
+    <message>
+        <source>Log in</source>
+        <translation>Увійти</translation>
+    </message>
+    <message>
+        <source>Log in to your XMPP account</source>
+        <translation>Увійдіть до вашого облікового запису XMPP</translation>
+    </message>
+    <message>
+        <source>Your Jabber-ID:</source>
+        <translation>Ваш ідентифікатор Jabber:</translation>
+    </message>
+    <message>
+        <source>user@example.org</source>
+        <translation>користувач@example.org</translation>
+    </message>
+    <message>
+        <source>Your Password:</source>
+        <translation>Ваш пароль:</translation>
+    </message>
+    <message>
+        <source>Connecting…</source>
+        <translation>Встановлення з&apos;єднання…</translation>
+    </message>
+    <message>
+        <source>Connect</source>
+        <translation>З&apos;єднатися</translation>
+    </message>
+    <message>
+        <source>Invalid username or password.</source>
+        <translation>Некоректне ім’я користувача або пароль.</translation>
+    </message>
+    <message>
+        <source>Cannot connect to the server. Please check your internet connection.</source>
+        <translation>Не вдалося встановити з&apos;єднання із сервером. Будь ласка, перевірте, чи маєте ви з&apos;єднання із інтернетом.</translation>
+    </message>
+    <message>
+        <source>The server doesn&apos;t support secure connections.</source>
+        <translation>На сервері не передбачено можливості захищених з&apos;єднань.</translation>
+    </message>
+    <message>
+        <source>Error while trying to connect securely.</source>
+        <translation>Помилка під час спроби встановлення захищеного з&apos;єднання.</translation>
+    </message>
+    <message>
+        <source>Could not resolve the server&apos;s address. Please check your JID again.</source>
+        <translation>Не вдалося визначити адресу сервера. Будь ласка, перевірте, чи правильно вказано ідентифікатор Jabber.</translation>
+    </message>
+    <message>
+        <source>Could not connect to the server.</source>
+        <translation>Не вдалося встановити з’єднання із віддаленим сервером.</translation>
+    </message>
+    <message>
+        <source>Authentification protocol not supported by the server.</source>
+        <translation>На сервері не передбачено підтримки протоколу розпізнавання.</translation>
+    </message>
+    <message>
+        <source>An unknown error occured; see log for details.</source>
+        <translation>Сталася невідома помилка. Будь ласка, ознайомтеся із журналом.</translation>
+    </message>
+</context>
+<context>
+    <name>MessageHandler</name>
+    <message>
+        <source>Could not send message, as a result of not being connected.</source>
+        <translation>Не вдалося надіслати повідомлення через те, що немає з&apos;єднання.</translation>
+    </message>
+    <message>
+        <source>Could not correct message, as a result of not being connected.</source>
+        <translation>Не вдалося виправити повідомлення через те, що немає з&apos;єднання.</translation>
+    </message>
+    <message>
+        <source>Spoiler</source>
+        <translation>Спойлер</translation>
+    </message>
+    <message>
+        <source>Message could not be sent.</source>
+        <translation>Повідомлення не вдалося надіслати.</translation>
+    </message>
+    <message>
+        <source>Message correction was not successful.</source>
+        <translation>Спроба виправити повідомлення зазнала невдачі.</translation>
+    </message>
+</context>
+<context>
+    <name>RegistrationManager</name>
+    <message>
+        <source>Password changed successfully.</source>
+        <translation>Пароль успішно змінено.</translation>
+    </message>
+    <message>
+        <source>Failed to change password: %1</source>
+        <translation>Не вдалося змінити пароль: %1</translation>
+    </message>
+</context>
+<context>
+    <name>RosterAddContactSheet</name>
+    <message>
+        <source>Add new contact</source>
+        <translation>Додати новий контакт</translation>
+    </message>
+    <message>
+        <source>This will also send a request to access the presence of the contact.</source>
+        <translation>Унаслідок цього також буде надіслано запит щодо доступу до даних щодо присутності контакту у системі.</translation>
+    </message>
+    <message>
+        <source>Jabber-ID:</source>
+        <translation>Ідентифікатор Jabber:</translation>
+    </message>
+    <message>
+        <source>user@example.org</source>
+        <translation>користувач@example.org</translation>
+    </message>
+    <message>
+        <source>Nickname:</source>
+        <translation>Псевдонім:</translation>
+    </message>
+    <message>
+        <source>Optional message:</source>
+        <translation>Додаткове повідомлення:</translation>
+    </message>
+    <message>
+        <source>Tell your chat partner who you are.</source>
+        <translation>Повідомте вашому співрозмовнику дещо про вас.</translation>
+    </message>
+    <message>
+        <source>Cancel</source>
+        <translation>Скасувати</translation>
+    </message>
+    <message>
+        <source>Add</source>
+        <translation>Додати</translation>
+    </message>
+</context>
+<context>
+    <name>RosterListItem</name>
+    <message>
+        <source>Error: Please check the JID.</source>
+        <translation>Помилка: будь ласка, перевірте, чи правильно вказано ідентифікатор Jabber.</translation>
+    </message>
+    <message>
+        <source>Available</source>
+        <translation>Доступний</translation>
+    </message>
+    <message>
+        <source>Free for chat</source>
+        <translation>Вільний для спілкування</translation>
+    </message>
+    <message>
+        <source>Away</source>
+        <translation>Відсутність</translation>
+    </message>
+    <message>
+        <source>Do not disturb</source>
+        <translation>Не турбувати</translation>
+    </message>
+    <message>
+        <source>Away for longer</source>
+        <translation>Довга відсутність</translation>
+    </message>
+    <message>
+        <source>Offline</source>
+        <translation>Поза мережею</translation>
+    </message>
+    <message>
+        <source>Error</source>
+        <translation>Помилка</translation>
+    </message>
+</context>
+<context>
+    <name>RosterManager</name>
+    <message>
+        <source>Could not add contact, as a result of not being connected.</source>
+        <translation>Не вдалося додати контакт через те, що немає з&apos;єднання.</translation>
+    </message>
+    <message>
+        <source>Could not remove contact, as a result of not being connected.</source>
+        <translation>Не вдалося вилучити контакт через те, що немає з&apos;єднання.</translation>
+    </message>
+    <message>
+        <source>Spoiler</source>
+        <translation>Спойлер</translation>
+    </message>
+</context>
+<context>
+    <name>RosterPage</name>
+    <message>
+        <source>Connecting…</source>
+        <translation>Встановлення з’єднання…</translation>
+    </message>
+    <message>
+        <source>Contacts</source>
+        <translation>Контакти</translation>
+    </message>
+    <message>
+        <source>Add new contact</source>
+        <translation>Додати новий контакт</translation>
+    </message>
+    <message>
+        <source>Offline</source>
+        <translation>Поза мережею</translation>
+    </message>
+</context>
+<context>
+    <name>RosterRemoveContactSheet</name>
+    <message>
+        <source>Do you really want to delete the contact &lt;b&gt;%1&lt;/b&gt; from your roster?</source>
+        <translation>Ви справді хочете вилучити запис контакту &lt;b&gt;%1&lt;/b&gt; із вашої книги записів контактів?</translation>
+    </message>
+    <message>
+        <source>Delete contact</source>
+        <translation>Вилучити контакт</translation>
+    </message>
+    <message>
+        <source>Cancel</source>
+        <translation>Скасувати</translation>
+    </message>
+    <message>
+        <source>Delete</source>
+        <translation>Вилучити</translation>
+    </message>
+</context>
+<context>
+    <name>SendMediaSheet</name>
+    <message>
+        <source>Caption</source>
+        <translation>Підпис</translation>
+    </message>
+    <message>
+        <source>Cancel</source>
+        <translation>Скасувати</translation>
+    </message>
+    <message>
+        <source>Send</source>
+        <translation>Надіслати</translation>
+    </message>
+</context>
+<context>
+    <name>SettingsPage</name>
+    <message>
+        <source>Settings</source>
+        <translation>Параметри</translation>
+    </message>
+    <message>
+        <source>Change password</source>
+        <translation>Змінити пароль</translation>
+    </message>
+    <message>
+        <source>Changes your account&apos;s password. You will need to re-enter it on your other devices.</source>
+        <translation>Змінити пароль до вашого облікового запису. Вам доведеться повторно ввести цей пароль на усіх інших ваших пристроях для спілкування.</translation>
+    </message>
+</context>
+<context>
+    <name>SubRequestAcceptSheet</name>
+    <message>
+        <source>Subscription Request</source>
+        <translation>Запит щодо підписки</translation>
+    </message>
+    <message>
+        <source>You received a subscription request by &lt;b&gt;%1&lt;/b&gt;. If you accept it, the account will have access to your presence status.</source>
+        <translation>Вами отримано запити щодо підписки від &lt;b&gt;%1&lt;/b&gt;. Якщо ви приймете його, відповідний обліковий запис матиме доступ до вашого стану присутності у системі.</translation>
+    </message>
+    <message>
+        <source>Decline</source>
+        <translation>Відхилити</translation>
+    </message>
+    <message>
+        <source>Accept</source>
+        <translation>Прийняти</translation>
+    </message>
+</context>
+<context>
+    <name>UploadManager</name>
+    <message>
+        <source>Could not send file, as a result of not being connected.</source>
+        <translation>Не вдалося надіслати файл через те, що немає з&apos;єднання.</translation>
+    </message>
+    <message>
+        <source>File</source>
+        <translation>Файл</translation>
+    </message>
+</context>
+</TS>

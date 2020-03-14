eb54a19107d0f6bdcaa2f090de5b25d15370d00f
Linus Jahn <lnj@kaidan.im>
2020-03-14 22:07:21 +0100

    Remove duplicate of render-logos.sh

--- utils/render-logos.sh
+++ utils/render-logos.sh
@@ -2,25 +2,27 @@
 
 KAIDAN_SOURCES=$(dirname "$(readlink -f "${0}")")/..
 
-if [ ! -d "$KAIDAN_SOURCES/misc/android/res/mipmap-xxxhdpi" ]; then
-    echo "*****************************************"
-    echo "Rendering logos"
-    echo "*****************************************"
-    rendersvg() {
-        inkscape -z -e $2 -w $3 -h $3 $1
-        optipng -quiet $2
-    }
+echo "*****************************************"
+echo "Rendering logos"
+echo "*****************************************"
 
-    androidlogo() {
-        mkdir $KAIDAN_SOURCES/misc/android/res/mipmap-$1
-        rendersvg $KAIDAN_SOURCES/data/images/kaidan.svg "$KAIDAN_SOURCES/misc/android/res/mipmap-$1/icon.png" $2
-        rendersvg $KAIDAN_SOURCES/data/images/kaidan.svg "$KAIDAN_SOURCES/misc/android/res/mipmap-$1/logo.png" $(( $2 * 4 ))
-    }
+rendersvg() {
+    inkscape -z -e $2 -w $3 -h $3 $1 >/dev/null
+    optipng -quiet -o7 $2 >/dev/null
+    advpng -z4 $2 >/dev/null
+}
 
-    androidlogo ldpi 36
-    androidlogo mdpi 48
-    androidlogo hdpi 72
-    androidlogo xhdpi 96
-    androidlogo xxhdpi 144
-    androidlogo xxxhdpi 192
-fi
+androidlogo() {
+    echo "Rendering $KAIDAN_SOURCES/misc/android/res/mipmap-$1..."
+    mkdir -p $KAIDAN_SOURCES/misc/android/res/mipmap-$1
+    rendersvg $KAIDAN_SOURCES/data/images/kaidan.svg "$KAIDAN_SOURCES/misc/android/res/mipmap-$1/icon.png" $2
+    rendersvg $KAIDAN_SOURCES/data/images/kaidan.svg "$KAIDAN_SOURCES/misc/android/res/mipmap-$1/logo.png" $(( $2 * 4 ))
+}
+
+androidlogo ldpi 36
+androidlogo mdpi 48
+androidlogo hdpi 72
+androidlogo xhdpi 96
+androidlogo xxhdpi 144
+androidlogo xxxhdpi 192
+
